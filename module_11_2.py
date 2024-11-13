4f99e3835392686c6360d2045ac6ce8f03b39a0f
Alex <alivsaz@gmail.com>
2024-11-13 11:31:21 +0300

    Add files via upload

---
+++ module_11_2.py
@@ -0,0 +1,53 @@
+# Интроспекция
+
+from pprint import pprint
+
+
+# КЛАСС и 2 ФУНКЦИИ созданы для примера
+class Human:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        if 0 < self.age < 7:
+            return f'{self.name} ребенок'
+        elif 7 <= self.age < 18:
+            return f'{self.name} учащийся'
+        elif 18 <= self.age < 65:
+            return f'{self.name} рабочий'
+        else:
+            return f'{self.name} пенсионер'
+
+def name_string(name='alex'):
+    return str(name)
+
+def name_age(age=25):
+    return age
+
+input_name = name_string(input('Введите имя: '))
+input_age = name_age(input('Введите возраст: '))
+human = Human(input_name, input_age)
+
+
+# ОСНОВНОЕ ЗАДАНИЕ
+
+def introspection_info(object):
+    dict_info = {}
+    dict_info['ID'] = id(object)
+    dict_info['type'] = type(object)
+    dict_info['attributes'] = object.__getattribute__
+    dict_info['methods'] = dir(object)
+    if hasattr(object, '__module__'):
+        dict_info['module'] = object.__module__
+    return dict_info
+
+str_info = introspection_info(input_name)
+pprint(str_info)
+func_info = introspection_info(name_string)
+pprint(func_info)
+func_class_info = introspection_info(human.__str__)
+pprint(func_class_info)
+class_info = introspection_info(human)
+pprint(class_info)
